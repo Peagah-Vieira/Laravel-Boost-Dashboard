6e03c77ae3fcad1f402a2fe3d1a96779fc4135e2
Pedro Henrique Vieira <peagahvieira2003@gmail.com>
2024-07-29 06:42:57 -0300

    :sparkles: feat: Booster model and migration

---
+++ app/Models/Booster.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Booster extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'first_name',
+        'last_name',
+        'phone',
+        'battlenet',
+        'discord',
+    ];
+}
